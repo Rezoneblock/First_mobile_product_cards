10a63bbe576dba6a3a4163268f79df90f71ee8be
Timur <trew.tr2019@yandex.ru>
2021-05-16 13:36:58 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+  for (item of $('.main__list__item')) {
+    // Если времён больше 4
+    if ($(item).find('.main__details__description__time-list').children().length > 4) {
+      // Скрываем все времена начиная с 4 включительно.
+      for (
+        let i = 3;
+        i < $(item).find('.main__details__description__time-list').children().length;
+        i++
+      ) {
+        $(item)
+          .find('.main__details__description__time-list')
+          .children()
+          [i].classList.add('main__details__description__time-list__link--hidden');
+      }
+
+      // Создаём кнопку "Ещё..." и ставим её ко всем временам.
+      $('<span class="main__details__description__time-list__more">Ещё...</span>').appendTo(
+        $(item).find('.main__details__description__time-list'),
+      );
+    }
+  }
+
+  // При клике на кнопку "Ещё...".
+  $('.main__details__description__time-list__more').on('click', function () {
+    // Скрываем саму кнопку "Ещё...".
+    this.classList.add('main__details__description__time-list__link--hidden');
+
+    // Находим все времена и удаляем у них скрывающий класс.
+    $(this)
+      .closest('.main__details__description__time-list')
+      .find('.main__details__description__time-list__link')
+      .removeClass('main__details__description__time-list__link--hidden');
+
+    // Даём место новым временам.
+    $(this)
+      .closest('.main__details__description__item')
+      .addClass('main__details__description__item--column');
+
+    // Меняем свойства списка времён.
+    $(this)
+      .closest('.main__details__description__time-list')
+      .addClass('main__details__description__time-list--wrap');
+
+    // Делаем вертикальные отступы.
+    $('.main__details__description__time-list__link').addClass(
+      'main__details__description__time-list__link--column',
+    );
+  });
+});
